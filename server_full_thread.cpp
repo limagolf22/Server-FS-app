b57eb6e8c1a34cdbb1f9a3906c77ee6860fd9e11
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-06-13 00:53:36 +0200

    Add files via upload

---
+++ server_full_thread.cpp
@@ -0,0 +1,319 @@
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_THREAD_
+
+#include <websocketpp/config/asio_no_tls.hpp>
+
+#include <websocketpp/server.hpp>
+
+#include <set>
+
+#include <websocketpp/common/thread.hpp>
+#include <windows.h>
+#include <stdio.h>
+
+#include <iostream>
+#include <sim_server.h>
+#pragma comment(lib, "user32.lib")
+
+#include "SimConnect.h"
+
+
+#define BUF_SIZE 256
+
+
+
+
+char STR_ENVOI_RPOS[250];
+char STR_ENVOI_RREF[250];
+
+std::mutex m_values_lock;
+HANDLE hSimConnect = NULL;
+int quit=0;
+RPOS_resp RPOS_VAL;
+RREF_resp RREF_VAL;
+
+Simthrottle tc;
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+using websocketpp::lib::thread;
+using websocketpp::lib::mutex;
+using websocketpp::lib::lock_guard;
+using websocketpp::lib::unique_lock;
+using websocketpp::lib::condition_variable;
+
+/* on_open insert connection_hdl into channel
+ * on_close remove connection_hdl from channel
+ * on_message queue send to all channels
+ */
+
+
+
+websocketpp::frame::opcode::value opcode_client = websocketpp::frame::opcode::value::TEXT;
+bool check_co = false;
+
+enum action_type {
+    SUBSCRIBE,
+    UNSUBSCRIBE,
+    MESSAGE
+};
+
+struct action {
+    action(action_type t, connection_hdl h) : type(t), hdl(h) {}
+    action(action_type t, connection_hdl h, server::message_ptr m)
+      : type(t), hdl(h), msg(m) {}
+
+    action_type type;
+    websocketpp::connection_hdl hdl;
+    server::message_ptr msg;
+};
+
+void send_a_message(server* s, websocketpp::connection_hdl hdl, std::string payload);
+void float2Bytes(char bytes_temp[5],float float_variable);
+
+class broadcast_server {
+    public:
+    broadcast_server() {
+        // Initialize Asio Transport
+        m_server.init_asio();
+
+        m_server.set_access_channels(websocketpp::log::alevel::none);
+        m_server.clear_access_channels(websocketpp::log::alevel::all);
+        // Register handler callbacks
+        m_server.set_open_handler(bind(&broadcast_server::on_open,this,::_1));
+        m_server.set_close_handler(bind(&broadcast_server::on_close,this,::_1));
+        m_server.set_message_handler(bind(&broadcast_server::on_message,this,::_1,::_2));
+    }
+
+    void run(uint16_t port) {
+        // listen on specified port
+        m_server.listen(port);
+
+        // Start the server accept loop
+        m_server.start_accept();
+
+        // Start the ASIO io_service run loop
+        try {
+            m_server.run();
+        } catch (const std::exception & e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    void on_open(connection_hdl hdl) {
+        {
+            check_co = true;
+            lock_guard<mutex> guard(m_action_lock);
+           // std::cout << "on_open" << std::endl;
+            m_actions.push(action(SUBSCRIBE,hdl));
+        }
+        m_action_cond.notify_one();
+    }
+
+    void on_close(connection_hdl hdl) {
+        {
+            lock_guard<mutex> guard(m_action_lock);
+            //std::cout << "on_close" << std::endl;
+            m_actions.push(action(UNSUBSCRIBE,hdl));
+        }
+        m_action_cond.notify_one();
+    }
+
+    void on_message(connection_hdl hdl, server::message_ptr msg) {
+        // queue message up for sending by processing thread
+        {
+            lock_guard<mutex> guard(m_action_lock);
+            //std::cout << "on_message" << std::endl;
+            m_actions.push(action(MESSAGE,hdl,msg));
+        }
+        m_action_cond.notify_one();
+    }
+
+
+    void process_messages() {
+        bool check=true;
+        while(check) {
+            unique_lock<mutex> lock(m_action_lock);
+
+            while(m_actions.empty()) {
+                m_action_cond.wait(lock);
+            }
+
+            action a = m_actions.front();
+            m_actions.pop();
+
+            lock.unlock();
+
+            if (a.type == SUBSCRIBE) {
+                std::cout <<"\nconnexion a un client\n";
+                lock_guard<mutex> guard(m_connection_lock);
+                m_connections.insert(a.hdl);
+            } else if (a.type == UNSUBSCRIBE) {
+                std::cout << "\ndeconnexion d'un client\n";
+                lock_guard<mutex> guard(m_connection_lock);
+                m_connections.erase(a.hdl);
+            } else if (a.type == MESSAGE) {
+                std::cout << "\n" << a.msg->get_payload() <<std::endl; // affichage du message
+                parser(a.msg->get_payload());
+            } else {
+                // undefined.
+            }
+        }
+        return;
+    }
+
+    void send_messages() {
+        while(!check_co) {
+            Sleep(10);
+        }
+        while (check_co) {
+
+            lock_guard<mutex> guard(m_connection_lock);
+
+            con_list::iterator it;
+            if (m_connections.empty()) {
+            }
+            else {
+                for (it = m_connections.begin(); it != m_connections.end(); ++it) {
+                    send_a_message(&m_server,*it,"50");
+                }
+            }
+            Sleep(50);
+        }
+        return;
+    }
+
+    void process_simconnect() {
+        if (initSimEvents()) {
+            while (quit == 0) {
+                    // Continuously call SimConnect_CallDispatch until quit - MyDispatchProc1 will handle simulation events
+                    SimConnect_CallDispatch(hSimConnect, MyDispatchProc1, NULL);
+                    Sleep(25);
+                }                
+            hr = SimConnect_Close(hSimConnect);
+            return;
+        }
+        else {
+            std::cout << "\nFailed to Connect!!!!\nPassing to test values...\n";
+            int q;
+            while (true) {
+                m_values_lock.lock();
+
+                RPOS_VAL.heading = q*8;
+                RPOS_VAL.elevationASL = 960+q;
+                RPOS_VAL.elevationAGL = 900+q;
+                RPOS_VAL.roll = q-20;
+
+                RREF_VAL.speed = 100+(q-20);
+                RREF_VAL.rpm = 1500 +(q-20);
+
+                m_values_lock.unlock();
+                q=(q+1)%41;
+                Sleep(50);
+            };
+            return;
+            
+        }
+    }
+
+    private:
+    typedef std::set<connection_hdl,std::owner_less<connection_hdl> > con_list;
+
+    server m_server;
+    con_list m_connections;
+    std::queue<action> m_actions;
+
+    mutex m_action_lock;
+    mutex m_connection_lock;
+    condition_variable m_action_cond;
+};
+
+void send_RPOS(server* s, websocketpp::connection_hdl hdl) {
+    char cache_RPOS[4][5];
+    m_values_lock.lock();
+    float floats_RPOS[4] = {RPOS_VAL.heading,RPOS_VAL.elevationASL,RPOS_VAL.elevationAGL,RPOS_VAL.roll};
+    m_values_lock.unlock();
+    int i;
+    for (i=0; i<4; i++){
+        float2Bytes(cache_RPOS[i],floats_RPOS[i]);
+    }
+    sprintf(STR_ENVOI_RPOS,"%d %4s%4s%4s%4s",(char)0,cache_RPOS[0],cache_RPOS[1],cache_RPOS[2],cache_RPOS[3]);
+
+    s->send(hdl, STR_ENVOI_RPOS, opcode_client);
+}
+
+void send_RREF(server* s, websocketpp::connection_hdl hdl) {
+    char cache_RREF[2][5];
+    m_values_lock.lock();
+    float floats_RREF[2] = {RREF_VAL.speed,RREF_VAL.rpm};
+    m_values_lock.unlock();
+    int i;
+    for (i=0; i<4; i++){
+        float2Bytes(cache_RREF[i],floats_RREF[i]);
+    }
+    sprintf(STR_ENVOI_RREF,"%d %4s%4s",(char)1,cache_RREF[0],cache_RREF[1]);
+
+    s->send(hdl, STR_ENVOI_RREF, opcode_client);
+}
+
+bool one_err=false;
+void send_a_message(server* s, websocketpp::connection_hdl hdl, std::string payload) {
+    try {
+        one_err=false;
+        send_RPOS(s,hdl);
+        send_RREF(s,hdl);
+
+     //   std::cout << "\rvaleurs envoyees : " << STR_ENVOI;
+    } catch (websocketpp::exception const & e) {
+        m_values_lock.unlock();
+        if (!one_err) {
+            one_err=true;
+            //std::cout << "Echo failed because: "<< "(" << e.what() << ")\n";
+        }
+
+    }
+}
+
+void float2Bytes(char bytes_temp[5],float float_variable){ 
+  memcpy(bytes_temp, (unsigned char*) (&float_variable), 4);
+}
+
+
+int main(int argc, char *argv[]) {
+    printf("initilisation du serveur\n");
+    int nport;
+    if (argc > 1) {
+        nport=std::stoi(argv[1]);
+    }
+    else {
+        nport=9002;
+    }
+
+    try {
+    broadcast_server server_instance;
+
+    // Start a thread to run the processing loop  
+    thread t(bind(&broadcast_server::process_messages,&server_instance));
+    thread t2(bind(&broadcast_server::send_messages,&server_instance));
+    thread t3(bind(&broadcast_server::process_simconnect,&server_instance));
+
+    printf("attente de connexion\n");
+    // Run the asio loop with the main thread
+    server_instance.run(nport);
+
+    t.join();
+    printf("sortie d'un thread\n");
+    t2.join();
+    printf("sortie deux threads\n");
+    t3.join();
+    printf("sortie trois threads\n");
+    } catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
---
+++ simconnect.cpp
@@ -0,0 +1,158 @@
+#include <string>
+#include <vector>
+#include <windows.h>
+#include "SimConnect.h"
+#include "sim_server.h"
+
+#include <map>
+using namespace std;
+
+map<string,int> Reqs;
+map<string,int> Defs;
+
+void add_Req(string name) {
+    int nbr = Reqs.size();
+    Defs[name] = nbr;
+  //  Defs.insert(pair(name,nbr));
+    return;
+}
+
+void add_Def(string name) {
+    int nbr = Defs.size();
+    Defs[name] = nbr;
+  //  Defs.insert(pair(name,nbr));
+    return;
+}
+
+void add_data(string def, char name[], char unit[], SIMCONNECT_DATATYPE datatype=SIMCONNECT_DATATYPE_FLOAT32) {
+    if ( Defs.find(def) == Defs.end() ) {
+        add_Def(def);
+    }
+    hr = SimConnect_AddToDataDefinition(hSimConnect, Defs[def], name, unit, datatype);
+    return;
+}
+
+void validate_req(string reqnum, string def, SIMCONNECT_PERIOD simper) {
+    hr = SimConnect_RequestDataOnSimObject(hSimConnect, Reqs[reqnum], Defs[def], SIMCONNECT_OBJECT_ID_USER, simper);
+    return;
+}
+
+
+Inputs isfired;
+Radpan radpan;
+
+void CALLBACK MyDispatchProc1(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext) {
+	switch (pData->dwID)
+	{
+
+	case SIMCONNECT_RECV_ID_SIMOBJECT_DATA:
+	{
+		SIMCONNECT_RECV_SIMOBJECT_DATA* pObjData = (SIMCONNECT_RECV_SIMOBJECT_DATA*)pData;
+
+		switch (pObjData->dwRequestID)
+		{
+        case REQUEST_RPOS:{
+            RPOS_resp* provi = (RPOS_resp*)&pObjData->dwData;
+            m_values_lock.lock();
+            RPOS_VAL.heading = provi->heading;
+            RPOS_VAL.elevationASL = provi->elevationASL;
+            RPOS_VAL.elevationASL = provi->elevationAGL;
+            RPOS_VAL.elevationASL = provi->roll;
+            m_values_lock.unlock();
+        }
+        case REQUEST_RREF:{
+            RREF_resp* provi = (RREF_resp*)&pObjData->dwData;
+            m_values_lock.lock();
+            RREF_VAL.speed = provi->speed;
+            RREF_VAL.rpm = provi->rpm;
+            m_values_lock.unlock();
+        }
+
+		}
+		break;
+	}
+
+	case SIMCONNECT_RECV_ID_QUIT:
+	{
+		quit = 1;
+		break;
+	}
+
+	default:
+		break;
+	}
+}
+
+enum GROUP_ID {
+    GROUP0,
+};
+
+
+HRESULT hr;
+int initSimEvents() {
+        
+        if (SUCCEEDED(SimConnect_Open(&hSimConnect, "Client Event Demo", NULL, 0, NULL, 0))) {
+            std::cout << "\nConnected To Microsoft Flight Simulator 2020!\n";
+
+            //DEFINITION <=> priority 
+            // DATA
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RPOS, "HEADING INDICATOR", "degrees", SIMCONNECT_DATATYPE_FLOAT32);
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RPOS, "Indicated Altitude", "feet", SIMCONNECT_DATATYPE_FLOAT32);
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RPOS, "Plane Alt Above Ground", "feet", SIMCONNECT_DATATYPE_FLOAT32);
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RPOS, "ATTITUDE INDICATOR BANK DEGREES", "degrees", SIMCONNECT_DATATYPE_FLOAT32);
+
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RREF, "Airspeed Indicated", "knots", SIMCONNECT_DATATYPE_FLOAT32);
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEF_RREF, "General Eng RPM:1", "Rpm", SIMCONNECT_DATATYPE_FLOAT32);
+
+
+            hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_BREAKDOWNS, "ENG ON FIRE", "Bool", SIMCONNECT_DATATYPE_INT32);
+
+            hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_RPOS, DEF_RPOS, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_VISUAL_FRAME);
+            hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_RREF, DEF_RREF, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_VISUAL_FRAME);
+
+            // Process incoming SimConnect Server messages
+            return 1;
+	    }
+        else {
+            return 0;
+        }
+}
+
+int parser(std::string strings) {
+    char *buffer = &strings[0];
+    COMMAND command;
+    char values[250];
+    sscanf(buffer,"%d %s", &command, values);
+    printf("datum:%d,value:%s\n",command,values);
+    switch (command) {
+        case ADD_REQ:{
+            char req[50];
+            sscanf(values, "%s",req);
+            string toSR(req);
+            add_Req(toSR);
+            printf("req %s added\n", req);
+        }
+
+        case ADD_VAL_DEF:{
+            char defi[50]; char name[50];char unit[50];SIMCONNECT_DATATYPE dtt;
+            sscanf(values, "[^;];[^;];[^;];%d",defi, name, unit, &dtt);
+            string toS(defi);
+            add_data(toS, name, unit, dtt);
+            printf("%s def has received the value %s\n",defi,name);
+
+        }
+        case DEF_REQ:{
+            char reqname[50]; char defi2[50]; int simper;
+            sscanf(values, "[^;];[^;];%d",reqname, defi2, &simper);
+            string toSreq(reqname);
+            string toSdef(defi2);
+            validate_req(toSreq, toSdef, (SIMCONNECT_PERIOD)simper);
+            
+
+        }
+
+       
+        return 0;
+    }
+    return 1;
+}
