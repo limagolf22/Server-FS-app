78f6538f00c8bdbcf7fe2e3d9602275713d4fce0
unknown <liam.guigo@gmail.com>
2021-06-13 00:56:07 +0200

    add include

---
+++ include/sim_server.h
@@ -0,0 +1,75 @@
+#ifndef __SIM_SERVER_H__
+#define __SIM_SERVER_H__
+
+#include <string>
+#include <windows.h>
+#include "SimConnect.h"
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex
+
+
+enum COMMAND {
+	ADD_REQ,
+	ADD_VAL_DEF,
+	DEF_REQ,
+};
+
+struct RPOS_resp {
+	float heading;
+	float elevationASL;
+	float elevationAGL;
+	float roll;
+};
+
+struct RREF_resp {
+	float speed;
+	float rpm;
+};
+
+enum DATA_DEFINE_ID {
+    DEFINITION_BREAKDOWNS,
+	DEFINITION_RADPANNELS,
+	DEF_RPOS,
+	DEF_RREF
+}; //ID de datum (set de datas)
+
+enum DATA_REQUEST_ID {
+	REQUEST_RADPANNEL,
+	REQUEST_2,
+	REQUEST_RPOS,
+	REQUEST_RREF
+}; //ID d'un request
+
+
+struct Inputs {
+    int32_t IsFired=0;
+};
+
+struct Radpan {
+    float VHF1 = 108;
+};
+
+void CALLBACK MyDispatchProc1(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext);
+int initSimEvents();
+int parser(std::string strings);
+
+extern int quit;
+extern HANDLE hSimConnect;
+
+extern RPOS_resp RPOS_VAL;
+extern RREF_resp RREF_VAL;
+
+extern std::mutex m_values_lock;
+
+extern HRESULT hr;
+
+extern Inputs isfired; 
+
+struct Simthrottle {
+    int32_t throttle;
+};
+extern Simthrottle tc;
+
+#endif
+
