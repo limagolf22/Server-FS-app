33873e3e3a22b2c0aa303dcccac17b35125f2d31
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-06-14 16:45:24 +0200

    Add files via upload

--- include/sim_server.h
+++ include/sim_server.h
@@ -13,6 +13,7 @@ enum COMMAND {
 	ADD_REQ,
 	ADD_VAL_DEF,
 	DEF_REQ,
+	TP_REQ
 };
 
 struct RPOS_resp {
@@ -27,11 +28,24 @@ struct RREF_resp {
 	float rpm;
 };
 
+struct Pos
+{
+    double  kohlsmann=29.92;
+    double  altitude=5000.0;
+    double  latitude;
+    double  longitude;
+	double 	roll=0.0;
+	double  pitch=0.0;
+	double  heading;
+	double speed=95.0;
+};
+
 enum DATA_DEFINE_ID {
     DEFINITION_BREAKDOWNS,
 	DEFINITION_RADPANNELS,
 	DEF_RPOS,
-	DEF_RREF
+	DEF_RREF,
+	DEF_POS
 }; //ID de datum (set de datas)
 
 enum DATA_REQUEST_ID {
